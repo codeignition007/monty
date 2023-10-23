da3dd6af14decb0eb84e81dff0fc326c5ed943cd
JOHNSON ADEOYE <johnsonadeoye888@gmail.com>
2023-10-22 18:44:48 -0700

    This is a 0x19. C - Stacks, Queues - LIFO, FIFO project

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a README file for the 0x19. C - Stacks, Queues - LIFO, FIFO project
---
+++ print_err.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * print_error - Prints error message to stderr
+ * @message: error message
+ * @file: exit status code
+ */
+void print_error(char *message, char *file)
+{
+	if (file == NULL)
+		file = "";
+	fprintf(stderr, "%s%s\n", message, file);
+	free(mont);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * malloc_error - prints malloc error
+ */
+void malloc_error(void)
+{
+	fprintf(stderr, "Error: malloc failed\n");
+	free(mont);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * print_line_number - prints unknown error:
+ * @line: line number
+ */
+void print_line_number(unsigned int line)
+{
+	fprintf(stderr, "L%d: unknown instruction %s\n", line, mont->arr[0]);
+}
